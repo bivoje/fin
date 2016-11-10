b8d595794a44c3dd718012459d78d7da132f368a
bivoje <bivoje49@gmail.com>
2016-11-10 18:22:03 +0900

    Goal #1 basic echo
    ifin will echo the stdin to stdout if it's piped

--- test.cpp
+++ test.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "fin.hpp"
 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  _ifin ifin(argc>1 && argv[1]==string("--piped"));
+
+  int n, m;
+  cout << "Enter : ";
+  ifin >> n >> '\n' >> m;
+  cout << "the value is " << n << ' ' << m << endl;
+
+  return 0;
+}
