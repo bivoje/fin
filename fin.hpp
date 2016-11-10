95b0e3cc97a408ddd4685b66a3e295c4cb563cb7
bivoje <bivoje49@gmail.com>
2016-11-10 18:40:10 +0900

    Created new version of class fin
    changed name from ifin to fin

--- fin.hpp
+++ fin.hpp
@@ -2,27 +2,66 @@
 
 using namespace std;
 
-class _ifin {
-  bool p;
-  char c;
-  const static char sc = ' ';
+class _fin {
+  bool empty; //is it moved?
+  bool h; //is this the first printing?
 public:
-  _ifin(bool _p) : p(_p), c(sc) {}
-  bool piped() {return p;}
-  _ifin& operator() (char _c) {c=_c; return *this;}
-  char token() {char tmp=c; c=sc; return tmp;}
+  const bool p;
+  const char c, e;
+  istream& i;
+  ostream& o;
+  //this is called when 'fin() >>'
+  _fin(bool _p, char _c, char _e, istream& _i, ostream& _o)
+    : empty(false), h(true), p(_p), c(_c), e(_e), i(_i), o(_o)
+  {
+    //cerr << "new _fin with " << bool(p) << ' ' << int(c) << ' ' << int(e) << endl;
+  }
+  //this is called when streaming(<<) happens
+  _fin(_fin&& f)
+    : empty(false), h(f.h), p(f.p), c(f.c), e(f.e), i(f.i), o(f.o)
+  {
+    f.empty = true;
+  }
+
+  bool first() {bool tb=h; h=false; return h;}
+
+  ~_fin()
+  {
+    if(!empty) //not moved, no more streaming
+      o << e;
+    //cerr << "_fin dead with " << bool(p) << ' ' << int(c) << ' ' << int(e) << endl;
+  }
 };
 
-template <class T>
-_ifin& operator>>(_ifin& i, T& x) 
-{
-  cin >> x;
-  if(i.piped())
-    cout << x << i.token();
-  return i;
-}
+class getfin {
+private:
+  const bool p;
+  istream& i;
+  ostream& o;
 
-_ifin& operator>>(_ifin& i, char c) 
+public:
+  getfin(bool _p, istream& _i=cin, ostream& _o=cout)
+    : p(_p), i(_i), o(_o) {}
+  _fin operator() (char c=' ', char e='\n')
+  { return _fin(p, c, e, i, o); }
+};
+
+//TODO what if we use rvalue at here? .. like
+//_fin&& operator>> (_fin&& f, T& x)
+template <class T>
+_fin operator>> (_fin f, T& x)
 {
-  return i(c);
+  //splitter
+  //do this before the element to prevent printing it out at the last
+  if(f.p && !f.first()) //if this is not the first of stream
+    f.o << f.c; //print the splitter
+
+  //get the value
+  f.i >> x;
+
+  //print the element
+  if(f.p)
+    f.o << x;
+
+  return f;
 }
