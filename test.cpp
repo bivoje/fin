76713b75fee9abfaa04cf559b9feeb62e02c3b24
bivoje <bivoje49@gmail.com>
2016-11-10 18:41:49 +0900

    test 3

--- test.cpp
+++ test.cpp
@@ -3,55 +3,29 @@
 
 using namespace std;
 
-//some other ways..
-
-//can not change the state after compilation.
-//_ifin ifin(char c) {
-//  static bool piped = true;
-//  return _ifin(c, piped);
-//}
-
-//changing the state only required once at the start of program
-//piped stated can not be altered while running..
-//if(argv[1] == string("piped"))
-//  ifin.on();
-//else
-//  ifin.off();
-
-int main(int argc, char** argv)
+int main()
 {
-  _ifin ifin(argc>1 && argv[1]==string("--piped"));
+  getfin fin(true);
 
-  //o... okay
-  int n, m;
-  cout << "Enter : ";
-  ifin >> n >> '\n' >> m;
-  cout << "the value is " << n << ' ' << m << endl;
+  int x, y, z;
+  cout << "Enter ";
+  fin() >> x >> y >> z;
+  cout << "Get " << x << ' ' << y << ' ' << z << endl;
 
-  //error
-  char c = '\n';
-  char n, m;
-  cout << "Enter : ";
-  ifin >> n >> c >> m;
-  cout << "the value is " << n << ' ' << m << endl;
+  int k[10];
+  //for(int i=0; i<10; i++)
+  //{
+  //  fin('\0','|') >> k[i];
+  //} fin('\0','\n');
 
-  //ifin.c is reset at every >>, 
-  //so if I want '\t' to split each field
-  ifin('\t') >> n >> '\t' >> m >> '\n' >> x;
-  //what I wanted to do is let the user to set splitter 
-  //every time like
-  cout << "insert : ";
-  ifin('\n') >> x;
-  cout << "number : ";
-  ifin('\n') >> n;
-  for(int i=0; i<n; i++)
-    ifin(' ') >> s;
-  //but it makes trouble when getting more than 2 at once.
-  //we can let ifin.c not reset, but ...
-  //it would cause frequent bug if programmer forget to set it.
-  ifin('\t') >> x >> y >> z; // split with tab
-  .... // some work
-  ifin >> n >> m; //still with tab.. 
+  //move constructor used
+  _fin f = fin('|','\n');
+  for(int i=0; i<10; i++)
+  {
+    //move constructor used
+    f = (f >> k[i]);
+  }
+  //not moved, destructor called
 
   return 0;
 }
